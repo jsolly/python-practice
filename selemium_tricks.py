b99615c6361734ef2482f72a07260fc863ed43aa
john8739 <jsolly@esri.com>
2021-05-05 17:29:19 -0700

    Wip

--- selemium_tricks.py
+++ selemium_tricks.py
@@ -3,6 +3,11 @@
 el = driver.find_element_by_xpath("//*[text()='Exact Match']")
 el = driver.find_element_by_xpath("//*[contains(text(), 'Partial Match')]")
 
+# This one seems to work better since it looks at inner nodes
+        self.driver.find_element_by_xpath(
+            ".//p[text() [contains(.,'cannot be found')]]"
+        )
+
 loginButton = browser.find_element(By.XPATH, "//*[@value='Log In']")
 
 
